de8515c4359e3588cd57db5229326476de9ea27c
jamiesandell <jamie.sandell@outlook.com>
2023-07-16 06:58:08 +0100

    Auth

---
+++ auth/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace MyProject;
+
+class Program
+{
+    private const string _clientId = "bf086018-f1eb-48a3-873f-a50ab641a0d8";
+    private const string _tenantId = "80294c2e-7d09-469f-95e7-d0e75197f067";
+
+    public static async Task Main(string[] args)
+    {
+        var app = PublicClientApplicationBuilder
+            .Create(_clientId)
+            .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+            .WithRedirectUri("http://localhost")
+            .Build();
+        
+        string[] scopes = {"user.read"};
+        AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+        Console.WriteLine($"Token:\t{result.AccessToken}");
+    }
+}
